b98233af899d3a61c83132855cba44c0bb960b79
Animesh Trivedi <Animesh.Trivedi@ibm.com>
2026-04-10 14:01:15 +0000

    passing the offloading length in the extra_body

--- vllm/benchmarks/datasets.py
+++ vllm/benchmarks/datasets.py
@@ -78,6 +78,7 @@ class SampleRequest:
     lora_request: LoRARequest | None = None
     request_id: str | None = None
     timestamp: float | None = None
+    offload_percentage: float | None = None
 
 
 # -----------------------------------------------------------------------------
@@ -1494,6 +1495,8 @@ class TimedTrace(BenchmarkDataset):
             prompt_ids = self._expand_prompt(
                 entry.get(self.label_hash_ids, []), input_length, tokenizer
             )
+            offload_lenght = entry.get("optimal_offload_tokens", input_length)
+            offload_percentage = (offload_lenght * 100) / input_length
             prompt = tokenizer.decode(prompt_ids)
 
             # Get timestamp with proper error handling
@@ -1517,6 +1520,7 @@ class TimedTrace(BenchmarkDataset):
                     multi_modal_data=None,
                     request_id=request_id_prefix + str(ind),
                     timestamp=timestamp,
+                    offload_percentage=offload_percentage,
                 )
             )
             ind += 1
